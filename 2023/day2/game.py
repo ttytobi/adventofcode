6817aa4c217075dbce136fe66635cbd11f7da3b9
ttytobi <tobias-hetzel@t-online.de>
2023-12-02 20:17:34 +0100

    day 2 - part2

--- 2023/day2/game.py
+++ 2023/day2/game.py
@@ -31,4 +31,42 @@ for line in raw:
     else:
         games.append(int(game_id.split()[-1]))
 
-print(sum(games))
+print(f'Part 1: {sum(games)}')
+
+# [
+# {'blue': 2, 'red': 3}, 
+# {'green': 3, 'blue': 3, 'red': 6}, 
+# {'blue': 4, 'red': 6}, 
+# {'green': 2, 'blue': 2, 'red': 9}, 
+# {'red': 2, 'blue': 4}
+# ]
+
+result = 0
+for line in raw:
+    joined = []
+    game_id, draw = line.split(':')
+    for subset in draw.split(';'):
+        dice_col = [] # [['3', 'green'], ['3', 'blue'], ['6', 'red']]
+        for dice in subset.split(','):
+            dice_col.append(dice.split())
+        dice_val = {} # {'green': 3, 'blue': 3, 'red': 6}
+        for y,x in dice_col:
+            dice_val[x] = int(y)
+        joined.append(dice_val)
+    # Go thru joined dicts. If blue, green, red is not in "sorted", add it. Then add the values to the list that is in each key
+    sorted = {}
+    for dicts in joined:
+        for key in dicts:
+            if key not in sorted:
+                sorted[key] = []
+            sorted[key].append(dicts[key])
+    # {'blue': [10, 12, 15, 15, 6], 'red': [8, 10, 17, 16, 1, 9], 'green': [12, 9, 3, 12, 6, 10]}
+    # For each key get the highest element of the list -> multiply -> sum up.
+    result += int(max(sorted["green"])) * int(max(sorted["red"])) * int(max(sorted["blue"]))
+
+print(f'Part 2: {result}')
+
+
+
+
+        
